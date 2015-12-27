0fb3ff447fd45e73d35b6f9de73e7fc40f296a3b
Suui <suui.gd@gmail.com>
2015-12-27 06:59:05 +0100

    Finished: Nancy Modules

---
+++ Pluralsight.NancyIntro/Course.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+
+namespace Pluralsight.NancyIntro
+{
+	public class Course
+	{
+		public int Id { get; set; } 
+		public string Name { get; set; }
+		public string Author { get; set; }
+
+		public Course(int id, string name, string author)
+		{
+			Id = id;
+			Name = name;
+			Author = author;
+		}
+
+		public static List<Course> List = new List<Course>
+		{
+			new Course(0, "Getting Started with Nancy", "Richard Cirerol"),
+			new Course(1, "HTTP Fundamentals", "Scott Allen")
+		};
+
+		public static int AddCourse(string name, string author)
+		{
+			var id = List.Count;
+			List.Add(new Course(id, name, author));
+
+			return id;
+		}
+	}
+}
---
+++ Pluralsight.NancyIntro/HomeModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Nancy;
+using Nancy.Responses;
+
+
+namespace Pluralsight.NancyIntro
+{
+	public class HomeModule : NancyModule
+	{
+		public HomeModule()
+		{
+			Func<Request, bool> isNotAnApiClient = request => !request.Headers.UserAgent.ToLower().StartsWith("curl");
+
+			Get["/", context => isNotAnApiClient.Invoke(context.Request)] = _ => View["index"];
+			Get["/"] = _ => "Welcome to the Pluralsight API";
+
+
+			/**
+			Course routes:
+			- GET list of courses
+			- GET single course
+			- POST single course
+			- Return responses as JSON
+			*/
+
+			Get["/courses"] = _ => new JsonResponse(Course.List, new DefaultJsonSerializer());
+
+			Get["/courses/{id}"] = parameter => Response.AsJson(Course.List.SingleOrDefault(x => x.Id == parameter.id));
+
+			Post["/courses"] = _ =>
+			{
+				var name = Request.Form.Name;
+				var author = Request.Form.Author;
+				var id = Course.AddCourse(name, author);
+
+				string url = string.Format("{0}/{1}", Context.Request.Url, id);
+
+				return new Response { StatusCode = HttpStatusCode.Accepted }
+					.WithHeader("Location", url);
+			};
+		} 
+	}
+}
